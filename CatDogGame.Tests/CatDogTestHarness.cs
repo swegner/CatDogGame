16708db47c70566009f6468eb715216abec2adf8
Scott Wegner <swegner@outlook.com>
2015-03-12 00:09:20 -0700

    Add missing test harness

---
+++ CatDogGame.Tests/CatDogTestHarness.cs
@@ -0,0 +1,13 @@
+﻿namespace InterviewQuestions.Tests
+{
+    public class CatDogTestHarness
+    {
+        public static bool TestTransformation(string from, string to)
+        {
+            var wordList = ThreeLetterWordList.Create();
+            var catDogGame = PrecomputedImplementation.Create(wordList);
+            
+            return catDogGame.HasValidTransformation(from, to);
+        }
+    }
+}
